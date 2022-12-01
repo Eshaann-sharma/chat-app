c7c9452c66a42445a19c7ac2f884d3b73f42aeb2
Eshaann-sharma <eshaansharma2244@gmail.com>
2022-12-02 01:03:14 +0530

    first commit

---
+++ PROJECT_x/project_x.py
@@ -0,0 +1,19 @@
+import threading
+import socket
+import time
+import tkinter
+
+def login():
+    x=input("Username : ")
+    pwd=input("Password : ")
+    
+    if pwd == '1' and x == '1' :
+        print("JNL")
+        
+    return 0
+    
+
+
+print("pROKECT X")
+login()
+
---
+++ PROJECT_x/server.py
@@ -0,0 +1,10 @@
+import socket
+import threading
+#import time
+
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) #getting local ip address
+
+print(SERVER)
+
+ 
